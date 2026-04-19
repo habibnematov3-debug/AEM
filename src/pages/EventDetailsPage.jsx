e0b49c982a2ebf7651924caa9e5c298c945caf98
khabib <habibnematov3@gmail.com>
2026-04-20 03:15:07 +0500

    Update EventDetailsPage.jsx

--- src/pages/EventDetailsPage.jsx
+++ src/pages/EventDetailsPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 
 import { useI18n } from '../i18n/LanguageContext'
 import { getLanguageLocale } from '../i18n/translations'
